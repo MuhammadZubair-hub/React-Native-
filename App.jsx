935c5d21de6ed655c5d3057d628d9f123c5c5e0a
muhammad zubair <mianmuhammadzubair94@gmail.com>
2025-01-16 12:58:05 +0500

    previous + image

--- App.jsx
+++ App.jsx
@@ -1,14 +1,22 @@
-import { Button, View, Text } from 'react-native'
+import { Button, View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
   return (
-    <View>
-      <Text  style={{fontSize:30}}>"App in the react native "</Text>
-      <Text style={{fontSize:30}}> "this e first app"</Text>
-      <Button title="pres here " style ={{Colors}}/>
+    <ScrollView>
+      <Text>IMAGE OF THE CAT </Text>
+      <View style={{justifyContent:'center', alignItems: 'center' ,flex :'2',padding: 10}}>
+      <Text  style={{fontSize:30, padding: 20}}>"Image of the cat  "</Text>
+      <Image source={{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png',}} style={{height:200,width:150}}></Image>
     </View>
+    <View style={{justifyContent:'center', alignItems: 'center' ,flex :'2',padding: 20}}>
+      <Text  style={{fontSize:30, padding: 20}}>"App in the react native "</Text>
+      <Text style={{fontSize:30}}> "this Is the first app"</Text>
+      <Button title="PRESS HERE  " style ={{padding: 40}}/>
+    </View>
+    </ScrollView>
+    
   );
 };
 
